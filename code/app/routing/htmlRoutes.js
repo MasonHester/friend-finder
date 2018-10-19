74ad81641cc58c39ce81db04684b4d0ca29871fe
Mason Hester <masonhester@Masons-MacBook-Pro.local>
2018-10-19 19:13:55 -0400

    Link to survey working

--- code/app/routing/htmlRoutes.js
+++ code/app/routing/htmlRoutes.js
@@ -1,11 +1,19 @@
 const path = require("path");
 
 module.exports = (app) => {
-    app.get("/survey", (req, res) => {
+    app.get("/quiz", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/html/survey.html"));
     });
 
-    app.get("*", (req, res) => {
+    app.get("/survey", (req, res) => {
+        res.redirect("/quiz");
+    })
+
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/html/home.html"));
     });
+
+    app.get("*", (req, res) => {
+        res.redirect("/");
+    })
 };
